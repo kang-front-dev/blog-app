94fdc2f25c4c302199051f88202d1727a280ddfd
Kostya Kan <kostikkan595@gmail.com>
2023-01-06 19:11:45 +0500

    fix: Some fixes

--- src/components/NavSearchCard.tsx
+++ src/components/NavSearchCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link,useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { IReview } from './classes/ReviewClass';
 
 interface ICardProps {
@@ -7,13 +7,15 @@ interface ICardProps {
   handleDelete: () => void;
 }
 export default function NavSearchCard({ cardInfo, handleDelete }: ICardProps) {
-  const navigate = useNavigate()
+
   return (
     <Link
       to={`review/${cardInfo._id}`}
       onClick={() => {
         handleDelete();
-        navigate(`review/${cardInfo._id}`)
+        setTimeout(() => {
+          window.location.reload()
+        }, 200);
       }}
     >
       <div
