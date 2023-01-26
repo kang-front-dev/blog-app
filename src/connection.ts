f704e320c160323f76fd30ff0fddf52309673aab
Kostya Kan <kostikkan595@gmail.com>
2023-01-26 17:17:02 +0500

    feat: Moved backend server from railway to cyclic

--- src/connection.ts
+++ src/connection.ts
@@ -1,2 +1,2 @@
-export const connectionUrl = 'https://blog-app-server-production-3ef5.up.railway.app'
+export const connectionUrl = 'https://rich-ruby-ray-cape.cyclic.app'
 // export const connectionUrl = 'http://localhost:5000'
